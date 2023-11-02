1b2fdccb6c27591a6c22968943cf55c29630a6f9
JesusCalleRuiz <146335396+JesusCalleRuiz@users.noreply.github.com>
2023-11-02 16:57:22 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ import addClients from "./resolvers/addClients.ts";
 import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
 const env = await load();
  
-const MONGO_URL = env.MONGO_URL || Deno.env.get("MONGO_URL");
+const MONGO_URL = "mongodb+srv://jesuscalleruiz:bXtR3dANxBRhh5Fk@cluster0.qeuki4l.mongodb.net/TiendaDB?retryWrites=true&w=majority";
 if (!MONGO_URL) {
   console.log("No mongo URL found");
   Deno.exit(1);
